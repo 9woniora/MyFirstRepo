8db7100579f7679bf0ee46f3004766fb20bb7e2f
Tony Ma <tony.ma@readify.net>
2020-04-20 13:52:15 +1000

    update Hello

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+import java.io.PrintStream;
+
+class Comments
+{
+
+
+	public static void main(String args[])
+	{
+		byte byte0 = 124;
+		System.out.println("hello b1");
+		System.out.println("99hello");
+	}
+}
